bc4502503dd2a6f9ac6bd6110ea2d648961087ca
Jeff Fowler <jeffowler@gmail.com>
2017-12-08 00:59:48 +0100

    moar

--- try.js
+++ try.js
@@ -1,20 +1,27 @@
 const
-err   = e => { throw error },
-car   = i => i[0],
-cdr   = i => i[1],
-first = car,
-rest  = i => i.slice(1,i.length),
-char = c => async i => first(i) == c ?  [c, rest(i)] : err(`error`),
-bind = (p1, p2) => (i) => p1(i).then((i2) => {
-    return p2(i2[1]).then((out) => [i2[0] + out[0], out[1]])
-}),
+  err   = e => { throw e },
+  car   = i => i[0],
+  cdr   = i => i[1],
+  first = car,
+  rest  = i => i.slice(1,i.length),
+  zero = async (i) => err('zero'),
+  item = async (i) => [first(i), rest(i)],
+  char = c => async i => first(i) == c ?  [c, rest(i)] : err(`error`),
+  and = (p1, p2) => (i) => p1(i).then(i2 => {
+      return p2(i2[1]).then((out) => [i2[0] + out[0], out[1]])
+  }),
+  or  = (p1, p2) => (i) => p1(i).catch(() => p2(i)),
+  map = (f, p) => (i) => p(i).then((r) => [f(car(r)), cdr(r)])
 
-a = char("a"),
-b = char("b"),
-c = char("c"),
-ab = bind(a, b),
-cc = bind(c, c),
-abcc = bind(ab, cc);
+  a = char("a"),
+  b = char("b"),
+  c = char("c"),
+  ab = and(a, b),
+  cc = and(c, c),
+  abcc = and(ab, cc);
+  abORcc = or(ab, cc);
+  one = char("1")
+  onetonum = map(parseInt, one)
 
-abcc('abccdjfio')
+onetonum('123')
 .then(console.log, console.log)
